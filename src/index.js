b1ec8fc169c8fd3463ec870b0f789cc5dcddbaaf
Usman Suleiman <usmansbk@gmail.com>
2021-07-25 12:50:20 +0100

    Display received data from api

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import './style.css';
 
 const GAME_NAME = 'usman';
 
+function displayScores(scores = []) {
+  const ul = document.getElementById('scores');
+  ul.innerHTML = '';
+
+  scores.forEach(({ user, score }) => {
+    const li = document.createElement('li');
+    li.innerText = `${user}: ${score}`;
+    ul.appendChild(li);
+  });
+}
+
 window.addEventListener('load', async () => {
   try {
     const id = await API.createGame(GAME_NAME);
-    console.log(await API.fetchScores(id));
+
+    const refreshButton = document.getElementById('refresh-button');
+    refreshButton.addEventListener('click', async () => {
+      const scores = await API.fetchScores(id);
+      displayScores(scores);
+    });
   } catch (error) {
     // Report error
   }
