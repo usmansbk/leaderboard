1bf52617c5a869b161675682a147e1fe8155cbd7
Usman Suleiman <usmansbk@gmail.com>
2021-07-25 12:42:43 +0100

    Add helper functions

--- src/api.js
+++ src/api.js
@@ -1,24 +1,17 @@
-import utils from './util.js';
-
-const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+import { parseId, post, get } from './util.js';
 
 export async function createGame(name) {
-  const response = await fetch(`${BASE_URL}games`, {
-    method: 'POST',
-    body: JSON.stringify({
-      name,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  const { result } = await response.json();
-  const id = utils.parseId(result);
+  const { result } = await post('games', { name });
+  const id = parseId(result);
   return id;
 }
 
 export async function fetchScores(id) {
-  const response = await fetch(`${BASE_URL}games/${id}/scores/`);
-  const { result } = await response.json();
+  const { result } = await get(`games/${id}/scores/`);
+  return result;
+}
+
+export async function submitScore(id, data) {
+  const { result } = await post(`games/${id}/scores/`, data);
   return result;
 }
--- src/util.js
+++ src/util.js
@@ -1,9 +1,24 @@
-function parseId(result = '') {
+const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+export function parseId(result = '') {
   const startIndex = 'Game with ID: '.length - 1;
   const endIndex = result.lastIndexOf(' ');
   return result.substring(startIndex, endIndex + 1);
 }
 
-export default {
-  parseId,
-};
+export async function post(URL, body) {
+  const response = await fetch(`${BASE_URL}${URL}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  return response.json();
+}
+
+export async function get(URL) {
+  const response = await fetch(`${BASE_URL}${URL}`);
+  return response.json();
+}
