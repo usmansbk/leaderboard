7512eefc35132f8b60ad842ec2837ff4bd4433e6
Usman Suleiman <usmansbk@gmail.com>
2021-07-25 09:36:03 +0100

    Add name label

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './style.css';
 
 window.addEventListener('load', () => {
   const ul = document.getElementById('scores');
-  new Array(10).fill(0).forEach((score, index) => {
+  new Array(10).fill(0).forEach((_, index) => {
     const li = document.createElement('li');
-    li.innerText = index;
+    li.innerText = `Name: ${index}`;
     ul.appendChild(li);
   });
 });
